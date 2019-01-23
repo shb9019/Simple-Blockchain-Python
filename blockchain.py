09cd4235c165b19d26c7d8300c88ff391205d654
Sai Hemanth B <saihemanth9019@gmail.com>
2019-01-23 22:15:32 +0530

    Code new_transaction

--- blockchain.py
+++ blockchain.py
@@ -7,9 +7,20 @@ class Blockchain(object):
         # Creates a new block and adds it to the chain
         pass
 
-    def new_transaction(self):
-        # Adds a new transaction to the list of transactions
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
