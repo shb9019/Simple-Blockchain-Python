b5cf0c6424f2ae9e8a4430268a32d76b0d08b784
Sai Hemanth B <saihemanth9019@gmail.com>
2019-01-23 22:12:46 +0530

    Add Boiler Code for Blockchain.py

---
+++ blockchain.py
@@ -0,0 +1,22 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # Creates a new block and adds it to the chain
+        pass
+
+    def new_transaction(self):
+        # Adds a new transaction to the list of transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        # Hashes a block
+        pass
+
+    @property
+    def last_block(self):
+        # Returns the last block in a chain
+        pass
