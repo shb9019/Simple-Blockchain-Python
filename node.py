6025db53c15e1bfbfd07fadde1f37fa54c7622ec
Sai Hemanth B <saihemanth9019@gmail.com>
2019-01-23 22:55:04 +0530

    Add code for Mine

--- node.py
+++ node.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
 from blockchain import Blockchain
+import pprint
 
+pp = pprint.PrettyPrinter(indent=2)
 blockchain = Blockchain()
+
+def mine(miner):
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+        sender = "0",
+        recipient = miner,
+        amount = 1,
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    pp.pprint(response)
+    print('\n')
 
 blockchain.new_transaction(
     sender = 'A',
@@ -15,4 +42,10 @@ blockchain.new_transaction(
     amount = 10,
 )
 
-print(blockchain.chain)
+pp.pprint(blockchain.chain)
+print('\n')
+
+mine('A')
+
+pp.pprint(blockchain.chain)
+print('\n')
